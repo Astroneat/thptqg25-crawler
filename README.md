71e7b855b15c99442a8fd586cc8ad16862f21c37
Astroneat <datnvt2007@gmail.com>
2025-07-17 01:31:26 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cào điểm thi THPTQG 2025
+
+- Chỉnh `region` theo mã vùng cần cào
+- Hơi chậm
---
+++ main.py
@@ -0,0 +1,55 @@
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+from bs4 import BeautifulSoup
+import csv
+
+region = '04'
+num_ids = 14548
+
+fieldnames = ["SBD", "Toán", "Ngữ văn", "Ngoại ngữ", "Lịch sử", "Địa lý", "Sinh học", "Vật lý", "Hóa học", "Tin học", "Công nghệ Công nghiệp", "Công nghệ Nông nghiệp", "Giáo dục kinh tế và pháp luật", "Giáo dục công dân"]
+
+csvfile = open('results.csv', 'a', newline='')
+csvfile.truncate(0)
+writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+writer.writeheader()
+
+def search(start, end):
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
+    for i in range(start, end):
+        sbd = '\'' + region + '0' * (6 - len(str(i))) + str(i)
+        url = f'https://diemthi.vnexpress.net/index/detail/sbd/{sbd}/year/2025'
+        response = session.get(url)
+
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            score_div = soup.find("div", {"class": "o-detail-thisinh__diemthi"})
+            if score_div is None:
+                print(sbd, ": NULL")
+                continue
+            scores_raw = score_div.find("table").find("tbody").find_all("tr")
+            scores = {"SBD": sbd}
+            for tr in scores_raw:
+                tmp = tr.find_all("td")
+                subject = tmp[0].text.strip()
+                score = tmp[1].text.strip()
+                scores[subject] = score
+
+            print(scores)
+            writer.writerow(scores)
+
+            # if scores["Toán"] == 8.5 and scores["Ngữ văn"] == 8 and scores["Lịch sử"] == 9 and scores["Ngoại ngữ"] < 6:
+            #     print(sbd, scores)
+            # else:
+            #     print(sbd, ": Nope")
+
+def main():
+    search(1, num_ids + 1)
+
+if __name__ == '__main__':
+    main()
